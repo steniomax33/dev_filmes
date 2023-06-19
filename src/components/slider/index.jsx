fcdf86bf0e44b1528ec0451502130e0feb9bdc3d
Stenio Max <steniomax28@gmail.com>
2023-06-19 19:18:28 -0300

    Foi criado cards com carrosel

---
+++ src/components/slider/index.jsx
@@ -0,0 +1,29 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import { Container } from './styless'
+
+import Card from '../Card'
+
+function Slider({ info, title }) {
+    console.log(info, title)
+    return (
+        <Container>
+            <h2>{title}</h2>
+            <Swiper
+                grabCursor
+                spaceBetween={10}
+                slidesPerView={'auto'}
+                className="swiper"
+            >
+                {info.map((item, index) => (
+                    <SwiperSlide key={index}>
+                        <Card item={item} />
+                    </SwiperSlide>
+                ))}
+                
+            </Swiper>
+        </Container>
+    )
+}
+
+export default Slider
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -4,9 +4,15 @@ import { useState, useEffect } from 'react'
 import api from '../../services/api'
 import { Background, Container, Info, ContainerButtons, Poster } from './styles'
 import Button from '../../components/Button'
+import Slider from '../../components/slider'
+import { getImages } from '../../utils/getImages'
 
 function Home() {
     const [movie, setMovie] = useState()
+    const [topMovies, setTopMovies] = useState()
+    const [topSeries, setTopSeries] = useState()
+    const [popularSeries, setPopularSeries] = useState()
+    const [topPeople, setTopPeople] = useState()
 
     useEffect(() => {
         async function getMovies() {
@@ -17,14 +23,59 @@ function Home() {
             setMovie(results[0])
         }
 
+        async function getTopMovies() {
+            const {
+                data: { results }
+            } = await api.get('/movie/top_rated')
+            console.log(results)
+            setTopMovies(results)
+        }
+
+        async function getTopSeries() {
+            const {
+                data: { results }
+            } = await api.get('/tv/top_rated')
+            console.log(results)
+            setTopSeries(results)
+        }
+
+        async function getPopularSeries() {
+            const {
+                data: { results }
+            } = await api.get('/tv/popular')
+            console.log(results)
+            setPopularSeries(results)
+        }
+
+        async function getPopularSeries() {
+            const {
+                data: { results }
+            } = await api.get('/tv/popular')
+            console.log(results)
+            setPopularSeries(results)
+        }
+
+        async function getTopPeople() {
+            const {
+                data: { results }
+            } = await api.get('/person/popular')
+            console.log('top artistas' + results)
+            setTopPeople(results)
+        }
+
+
         getMovies()
+        getTopMovies()
+        getTopSeries()
+        getPopularSeries()
+        getTopPeople()
     }, [])
 
     return (
         <>
             {movie && (
                 <Background
-                    img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                    img={getImages(movie.backdrop_path)}
                 >
                     <Container>
                         <Info>
@@ -36,14 +87,17 @@ function Home() {
                             </ContainerButtons>
                         </Info>
                         <Poster>
-                            <img
-                                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                                alt="capa-do-filme"
+                            <img src={getImages(movie.poster_path)}
+                                 alt="capa-do-filme"
                             />
                         </Poster>
                     </Container>
                 </Background>
             )}
+            {topMovies && <Slider info={topMovies} title={'Top filmes'} />}
+            {topSeries && <Slider info={topSeries} title={'Top Séries'} />}
+            {popularSeries && <Slider info={popularSeries} title={'Séries Populares'} />}
+            {topPeople && <Slider info={topPeople} title={'Top Artistas'} />}
         </>
     )
 }
